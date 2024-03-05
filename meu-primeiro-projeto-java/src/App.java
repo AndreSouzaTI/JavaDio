416040b15cda59ae4e8e026564eeeb55932217d1
André Souza <andresouza_ti@yahoo.com>
2024-03-05 17:43:18 -0300

    Meu primeiro projeto

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Começamos, iniciando do básico ao desenvolvedor Java by DIO");
+    }
+}
